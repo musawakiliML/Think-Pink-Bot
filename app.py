81b76c125fd158f85e50ad4495c250582868f220
musawakiliML <musawakiliml@hotmail.com>
2022-09-22 16:23:24 +0100

    Updated

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 # Chatbot implementation
-
-from flask import Flask
+import re
+from urllib import response
+from flask import Flask, request, jsonify
 from twilio.twiml.messaging_response import MessagingResponse
+import utils
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-@app.route('/bot', methods=["POST"])
-def bot():
+
+@app.route('/')
+def index():
+
     return "Hello World!!"
 
+
+@app.route("/bot", methods=["POST"])
+def bot():
+    data = request.get_json()
+    print(data)
+    # if 'quote' in data['queryResult']['queryText']:
+    #    today_quote = utils.get_quote()
+    #    response = {
+    #        "fulfillmentText": today_quote
+   #     }
+    #if 'image' in data['queryResult']['queryText']:
+    link = 'https://sample-videos.com/img/Sample-jpg-image-100kb.jpg'
+
+    response = {
+        "fulfillmentMessages":
+            {
+                "payload": {
+                    'mediaUrl': link,
+                    'text': "Testing images"
+                }
+            }
+
+    }
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
--- utils.py
+++ utils.py
@@ -1,4 +1,18 @@
 # Chatbot Functions
 import requests
 
-response = requests.get()
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/today")
+
+    if response.status_code == 200:
+        values = response.json()
+        quote = values[0]['q']
+        author = values[0]['a']
+        
+        result = f"{author.upper()} Said: '{quote}'."
+        
+        #print(quote)
+        #print(author)
+        #print(values)
+        
+        return result
