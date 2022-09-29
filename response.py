ebf1f6e43ac4574bd7c7ac7fa65e9156b30ba88a
musawakiliML <musawakiliml@hotmail.com>
2022-09-29 18:17:41 +0100

    Added about us response

--- response.py
+++ response.py
@@ -2,4 +2,30 @@
 
 
 def about_us():
-    response_message = {}
+    response_message = {
+            "fulfillmentMessages": [
+                {
+                    'text':{
+                        'text':["Think Pink is Non-Governmental Organization that aims at raising awareness for breast cancer, supporting the individuals affected by it."]
+                    }
+                },
+                {
+                    'text':{
+                        'text':["Saving Lives Through early detection. Early diagnosis of breast cancer is possible through adequate awareness and intervention."]
+                    }
+                },
+                {
+                    'text':{
+                        'text':["Our Chatbot will help learn about breast cancer."]
+                    }
+                },
+                {
+                    "payload": {
+                        "mediaUrl":"#",
+                        "text": "We are Think Pink Breast Cancer Awareness"
+
+                        # // custom integration payload here
+                    }
+                }
+            ]
+        }
