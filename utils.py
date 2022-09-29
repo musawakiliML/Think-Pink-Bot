b6b2c63f91fd85ac2632c55fc023cbbf2ee25b6c
musawakiliML <musawakiliml@hotmail.com>
2022-09-29 17:54:41 +0100

    Created a sgoogle search function

--- utils.py
+++ utils.py
@@ -1,3 +1,23 @@
 # Chatbot Functions
+
+# External libraries
 import requests
-import 
+from googlesearch import search
+
+
+# Google Search function
+def search_google(user_message):
+    # results list
+    results = []
+
+    # searching and storing the urls
+    for i in search(user_message, lang='en', num_results=6):
+        results.append(i)
+    return results
+
+
+query = input("Enter a search query:")
+
+results = search_google(query)
+
+print(results)
